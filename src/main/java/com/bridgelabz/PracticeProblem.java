23c59c8e611de57f49fbc062cdfa4dd9ab7f0184
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-10 19:41:37 +0530

    Added: Default values of data types

--- src/main/java/com/bridgelabz/PracticeProblem.java
+++ src/main/java/com/bridgelabz/PracticeProblem.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
 public class PracticeProblem {
+    static boolean defaultValue1;
+    static double defaultValue2;
+    static float defaultValue3;
+    static int defaultValue4;
+    static long defaultValue5;
+    static String defaultValue6;
+
     public static void main(String[] args) {
-        System.out.println(" Hello World ");
+        System.out.println("Default values of primitive data types are as follows:");
+        System.out.println("Default values of boolean = " + defaultValue1);
+        System.out.println("Default values of double = " + defaultValue2);
+        System.out.println("Default values of float = " + defaultValue3);
+        System.out.println("Default values of int = " + defaultValue4);
+        System.out.println("Default values of long = " + defaultValue5);
+        System.out.println("Default values of string = " + defaultValue6);
     }
 }
