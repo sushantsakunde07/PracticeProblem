3c957c5b53d7be19a55eb900b3143ac73963abce
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-12 00:03:52 +0530

    Added: Name using Command line

--- src/main/java/com/bridgelabz/PracticeProblem.java
+++ src/main/java/com/bridgelabz/PracticeProblem.java
@@ -2,18 +2,8 @@ package com.bridgelabz;
 
 public class PracticeProblem {
     public static void main(String[] args) {
-        int sum = 0;
-        int count = 0;
-        System.out.println("Calculating Sum of command line Args Integers ");
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-            try {
-                sum = sum + Integer.parseInt(args[i]);
-            } catch (NumberFormatException a) {
-                count++;
-            }
-        }
-        System.out.println("Sum of integers : " + sum);
-        System.out.println("Count of non integers : " + count);
+        String string1;
+        string1 = args[0];
+        System.out.println(string1);
     }
 }
