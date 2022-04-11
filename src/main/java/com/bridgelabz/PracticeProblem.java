0473b40caa66721cabe0a9c5abced8496e891fc5
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-04-11 22:23:21 +0530

    Added: String equal or not

--- src/main/java/com/bridgelabz/PracticeProblem.java
+++ src/main/java/com/bridgelabz/PracticeProblem.java
@@ -1,7 +1,14 @@
 package com.bridgelabz;
-public class PracticeProblem {
 
+public class PracticeProblem {
     public static void main(String[] args) {
-        System.out.println(" Hello World ");
+        String string1 = "Hello World";
+        String string2 = "Hello World";
+        String string3 = "Hello Java ";
+        System.out.println("string 1 = " + string1 + " \nstring 2 = " + string2 + " \nstring 3 = " + string3);
+        boolean retval1 = string2.equals(string1);
+        boolean retval2 = string2.equals(string3);
+        System.out.println("string 2 is equal to string 1 = " + retval1);
+        System.out.println("string 2 is equal to string 3 = " + retval2);
     }
 }
